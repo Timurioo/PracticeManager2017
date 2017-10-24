6f0c9d5440f1e11e28f88c76db70e6529d1d34d3
ChekhDmitry <dimacheh97@mail.ru>
2017-10-24 12:14:01 +0300

    StudentsService realization created.

---
+++ backend/src/main/java/com/netcracker/pmbackend/impl/services/StudentsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.netcracker.pmbackend.impl.services;
+
+import com.netcracker.pmbackend.impl.entities.StudentsEntity;
+import com.netcracker.pmbackend.interfaces.StudentsService;
+import com.netcracker.pmbackend.repository.StudentsRepository;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service("jpaStudentsService")
+@Repository
+@Transactional
+public class StudentsServiceImpl implements StudentsService {
+
+    @Autowired
+    private StudentsRepository studentsRepository;
+
+
+    public List<StudentsEntity> findAll() {
+        return Lists.newArrayList(studentsRepository.findAll());
+    }
+
+
+    public List<StudentsEntity> findByName(String name) {
+        return studentsRepository.findByName(name);
+    }
+
+
+    public List<StudentsEntity> findByGroup(String group) {
+        return studentsRepository.findByGroup(group);
+    }
+}
