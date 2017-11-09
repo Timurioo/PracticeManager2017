6018f3c09657826428178a31d1cc429f2eac09d5
ChekhDmitry <dimacheh97@mail.ru>
2017-11-09 12:49:31 +0300

    DTO validators(Student, Head of practice) were added.

---
+++ frontend/src/main/java/com/netcracker/etalon/validation/validator/HeadOfPracticeRegistrationDTOValidator.java
@@ -0,0 +1,51 @@
+package com.netcracker.etalon.validation.validator;
+
+import com.netcracker.etalon.dto.HeadOfPracticeRegistrationDTO;
+import com.netcracker.pmbackend.interfaces.HeadofpracticesService;
+import com.netcracker.pmbackend.interfaces.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * Created by dima on 11/7/2017.
+ */
+@Component
+public class HeadOfPracticeRegistrationDTOValidator implements Validator {
+
+    @Autowired
+    private UsersService usersService;
+
+    @Autowired
+    private HeadofpracticesService headofpracticesService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return HeadOfPracticeRegistrationDTO.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        HeadOfPracticeRegistrationDTO headOfPracticeRegistrationDTO = (HeadOfPracticeRegistrationDTO) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"login","Should be not empty");
+        if(usersService.findByLogin(headOfPracticeRegistrationDTO.getLogin()) != null){
+            errors.rejectValue("login","Login has been already in use");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"password","Should be not empty");
+        if(usersService.findByPassword(headOfPracticeRegistrationDTO.getPassword()) != null){
+            errors.rejectValue("password","Password has been already in use");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"name","Should be not empty");
+        if(headofpracticesService.findByName(headOfPracticeRegistrationDTO.getName()) != null){
+            errors.rejectValue("name","Name has been already in use");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"role","NotEmpty");
+
+    }
+}
---
+++ frontend/src/main/java/com/netcracker/etalon/validation/validator/StudentRegistrationDTOValidator.java
@@ -0,0 +1,63 @@
+package com.netcracker.etalon.validation.validator;
+
+import com.netcracker.etalon.dto.StudentRegistrationDTO;
+import com.netcracker.pmbackend.interfaces.StudentsService;
+import com.netcracker.pmbackend.interfaces.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * Created by dima on 11/8/2017.
+ */
+@Component
+public class StudentRegistrationDTOValidator implements Validator {
+
+    @Autowired
+    private UsersService usersService;
+
+    @Autowired
+    private StudentsService studentsService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return StudentRegistrationDTO.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+
+        StudentRegistrationDTO studentRegistrationDTO = (StudentRegistrationDTO) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"login","Should be not empty");
+        if(usersService.findByLogin(studentRegistrationDTO.getLogin()) != null){
+            errors.rejectValue("login","Login has been already in use");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"password","Should be not empty");
+        if(usersService.findByPassword(studentRegistrationDTO.getPassword()) != null){
+            errors.rejectValue("password","Password has been already in use");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"email","Should be not empty");
+        if(studentsService.findByEmail(studentRegistrationDTO.getEmail()) != null){
+            errors.rejectValue("email","Email has been already in use");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"phone","Should be not empty");
+        if(studentsService.findByPhone(studentRegistrationDTO.getPhone()) != null){
+            errors.rejectValue("phone","Phone has been already in use");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"name","Should be not empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"surname","Should be not empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"group","Should be not empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"budget","Should be not empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"avrMark","Should be not empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"role","Should be not empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"facultyId","Should be not empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"specialityId","Should be not empty");
+    }
+}
