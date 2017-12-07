23fa19b77f72ff2c1afc8a5ea86b8566a935472d
ChekhDmitry <dimacheh97@mail.ru>
2017-12-07 23:30:02 +0300

    Quartz cron scheduling for assign students status check has been added.

--- backend/src/main/java/com/netcracker/pmbackend/schedule/ScheduledTasks.java
+++ backend/src/main/java/com/netcracker/pmbackend/schedule/ScheduledTasks.java
@@ -1,20 +1,55 @@
 package com.netcracker.pmbackend.schedule;
 
+import com.netcracker.pmbackend.impl.entities.AssignStudentsEntity;
+import com.netcracker.pmbackend.impl.entities.PracticesEntity;
+import com.netcracker.pmbackend.impl.entities.StudentsEntity;
+import com.netcracker.pmbackend.interfaces.AssignStudentsService;
+import com.netcracker.pmbackend.interfaces.PracticesService;
+import com.netcracker.pmbackend.interfaces.StudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
-/**
- * Created by dima on 12/7/2017.
- */
+
 @Component
 public class ScheduledTasks {
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        System.out.println("The time is now " + dateFormat.format(new Date()));
+    @Autowired
+    private AssignStudentsService assignStudentsService;
+
+    @Autowired
+    private StudentsService studentsService;
+
+    @Autowired
+    private PracticesService practicesService;
+
+    @Scheduled(cron = "0 0 0 * * *", zone="Europe/Minsk")
+    public void checkStudentStatus() {
+
+        List<AssignStudentsEntity> assignStudentsEntityList = assignStudentsService.findAll();
+
+        for (AssignStudentsEntity assignStudentsEntity: assignStudentsEntityList){
+
+            PracticesEntity practicesEntity = practicesService.findById(assignStudentsEntity.getPracticeId());
+            Date startDate = practicesEntity.getFirstDate();
+            Date finishDate = practicesEntity.getFinishDate();
+            Date currentDate = new Date();
+            if(currentDate.compareTo(startDate)<0){
+                updateStudentStatus(assignStudentsEntity.getStudentId(), "Waiting");
+            }else if(currentDate.compareTo(startDate)>=0 && currentDate.compareTo(finishDate)<=0){
+                updateStudentStatus(assignStudentsEntity.getStudentId(), "Busy");
+            }else{
+                updateStudentStatus(assignStudentsEntity.getStudentId(), "Passed");
+            }
+        }
+    }
+
+    private void updateStudentStatus(int studentId, String status){
+        StudentsEntity studentsEntity = studentsService.findById(studentId);
+        studentsEntity.setStatus(status);
+        studentsService.save(studentsEntity);
     }
 }
